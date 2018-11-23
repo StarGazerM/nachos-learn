17576cbf61d1bb9a95196cdf96af71b4d726d02a
stargazermiao <stargazermiao@gmail.com>
2018-11-23 17:36:36 -0500

    test fs

--- nachos/code/threads/threadtest.cc
+++ nachos/code/threads/threadtest.cc
@@ -5,11 +5,19 @@
 void
 SimpleThread(int which)
 {
-    int num;
+    //int num;
     
-    for (num = 0; num < 5; num++) {
-        printf("*** thread %d looped %d times\n", which, num);
-        kernel->currentThread->Yield();
+    // for (num = 0; num < 5; num++) {
+    //     printf("*** thread %d looped %d times\n", which, num);
+    //     kernel->currentThread->Yield();
+    // }
+    bool res = kernel->fileSystem->Create("test", 1024);
+    if(res)
+    {
+        cout << "success\n";
+    }
+    else{
+        cout << "fail\n";
     }
 }
 
@@ -19,5 +27,5 @@ ThreadTest()
     Thread *t = new Thread("forked thread");
     t->Fork((VoidFunctionPtr) SimpleThread, (void *) 1);
     
-    SimpleThread(0);
+    //SimpleThread(0);
 }
